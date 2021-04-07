fb9748329084bee97d6f59ffaac9859c8d06c443
chenceen <ccs19991208@gmail.com>
2021-04-07 03:05:32 -0700

    add FFT.py

--- exam1/FFT.py
+++ exam1/FFT.py
@@ -4,7 +4,7 @@ import serial
 import time
 
 Fs = 200.0;  # sampling rate
-Ts = 1.0/Fs; # sampling interval
+Ts = 1/Fs; # sampling interval
 t = np.arange(0,1,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
 y = np.arange(0,1,Ts) # signal vector; create Fs samples
 
